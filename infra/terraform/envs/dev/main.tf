202ef6391e0068f9e163ef148e57e1e0699bb8a0
np03042021 <94600928+np03042021@users.noreply.github.com>
2026-04-07 17:55:02 +0530

    Update main.tf

--- infra/terraform/envs/dev/main.tf
+++ infra/terraform/envs/dev/main.tf
@@ -7,10 +7,34 @@ resource "aws_ecr_repository" "app" {
   name = var.ecr_repo_name
 }
 
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "${var.cluster_name}-eks-cluster-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = { Service = "eks.amazonaws.com" },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_AmazonEKSClusterPolicy" {
+  role       = aws_iam_role.eks_cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+# Optional but common: if you enable EKS control plane logging / VPC controller features
+resource "aws_iam_role_policy_attachment" "eks_cluster_AmazonEKSVPCResourceController" {
+  role       = aws_iam_role.eks_cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+}
+
 # 2) EKS Cluster (simplified; in production use modules and proper VPC)
 resource "aws_eks_cluster" "this" {
   name     = var.cluster_name
-  role_arn = "arn:aws:iam::755663465576:role/GitHubActionsEKSDeployRole"
+  role_arn = aws_iam_role.eks_cluster_role.arn
 
 
   access_config {
