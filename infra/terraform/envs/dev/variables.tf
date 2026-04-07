994caac4ddc43607d5ea0ed87a34e84a6683e210
np03042021 <94600928+np03042021@users.noreply.github.com>
2026-04-07 17:14:19 +0530

    Update variables.tf

--- infra/terraform/envs/dev/variables.tf
+++ infra/terraform/envs/dev/variables.tf
@@ -3,3 +3,14 @@ variable "aws_region" {
   type        = string
   default     = "us-east-1"
 }
+
+variable "ecr_repo_name" {
+  description = "AWS region where resources will be created"
+  type        = string
+  default     = "spring-eks-demo"
+}
+variable "cluster_name" {
+  description = "AWS region where resources will be created"
+  type        = string
+  default     = "demo-eks"
+}
