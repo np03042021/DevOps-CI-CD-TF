f6d96c3345cd0627bb4711d2b5a5fc955b6c4e54
np03042021 <94600928+np03042021@users.noreply.github.com>
2026-04-07 16:13:10 +0530

    Update backend.tf

--- infra/terraform/envs/dev/backend.tf
+++ infra/terraform/envs/dev/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "my-tf-state-bucket"
     key            = "eks/dev/terraform.tfstate"
-    region         = "us-east-1"
+    region         = "us-west-2"
     dynamodb_table = "my-tf-lock"
     encrypt        = true
   }
