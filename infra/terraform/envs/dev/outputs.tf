2f82cf119092e584c935fcd2fce0fb16c4e8a9a8
np03042021 <94600928+np03042021@users.noreply.github.com>
2026-04-07 15:18:19 +0530

    Update outputs.tf

--- infra/terraform/envs/dev/outputs.tf
+++ infra/terraform/envs/dev/outputs.tf
@@ -1,1 +1,4 @@
-
+output "cluster_name" { value = aws_eks_cluster.this.name }
+output "cluster_region" { value = var.aws_region }
+output "ecr_repo_url" { value = aws_ecr_repository.app.repository_url }
+output "gha_role_arn" { value = aws_iam_role.github_actions.arn }
