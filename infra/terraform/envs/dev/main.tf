48f4bb9f076d3e8f6d6c11086b2d4b8d0039e196
np03042021 <94600928+np03042021@users.noreply.github.com>
2026-04-10 19:05:35 +0530

    Update main.tf

--- infra/terraform/envs/dev/main.tf
+++ infra/terraform/envs/dev/main.tf
@@ -208,7 +208,11 @@ resource "aws_eks_node_group" "default" {
   cluster_name    = aws_eks_cluster.this.name
   node_group_name = "${var.cluster_name}-ng"
   node_role_arn   = aws_iam_role.eks_node_role.arn
-  subnet_ids      = var.subnet_ids
+  subnet_ids = [
+                aws_subnet.subnet_1.id,
+                aws_subnet.subnet_2.id
+              ]
+
 
   scaling_config {
     desired_size = 2
