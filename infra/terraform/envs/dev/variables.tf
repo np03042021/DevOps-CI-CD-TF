0ff7eae9eb2554fc21d926091c5e26834501907d
np03042021 <94600928+np03042021@users.noreply.github.com>
2026-04-10 18:38:34 +0530

    Update variables.tf

--- infra/terraform/envs/dev/variables.tf
+++ infra/terraform/envs/dev/variables.tf
@@ -1,7 +1,7 @@
 variable "aws_region" {
   description = "AWS region where resources will be created"
   type        = string
-  default     = "ap-south-1"
+  default     = "us-east-1"
 }
 
 variable "ecr_repo_name" {
