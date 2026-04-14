f9821a608c393d4c1c92e298c53f88019958c3e7
np03042021 <94600928+np03042021@users.noreply.github.com>
2026-04-14 15:06:07 +0530

    Update backend.tf

--- infra/terraform/envs/dev/backend.tf
+++ infra/terraform/envs/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-tf-state-bucket-cicdmynew"
+    bucket         = "my-tf-state-bucket-cicdfinal"
     key            = "eks/dev/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
