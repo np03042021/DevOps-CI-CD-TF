ce3d5eb624b7ea2f2d473b23019b73d1a50e69b8
np03042021 <94600928+np03042021@users.noreply.github.com>
2026-04-20 11:28:27 +0530

    Delete infra/terraform/modules/eks/backend.tf

--- infra/terraform/modules/eks/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "my-tf-state-bucket-cicdfinal"
-    key            = "eks/dev/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-  }
-}
